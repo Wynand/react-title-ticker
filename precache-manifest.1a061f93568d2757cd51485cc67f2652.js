2f6d5ab2feea4d25648e4edde944e9d6992c035a
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-02-07 22:53:32 -0600

    Updates

--- precache-manifest.1a061f93568d2757cd51485cc67f2652.js
+++ precache-manifest.1a061f93568d2757cd51485cc67f2652.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/react-title-ticker/static/js/runtime~main.09e6e54f.js"
   },
   {
-    "revision": "8625e2e53af06e384ebb",
-    "url": "/react-title-ticker/static/js/main.8625e2e5.chunk.js"
+    "revision": "3f293cd9d156f2d9ca81",
+    "url": "/react-title-ticker/static/js/main.3f293cd9.chunk.js"
   },
   {
     "revision": "a40ee5cbe20af19a48ad",
     "url": "/react-title-ticker/static/js/1.a40ee5cb.chunk.js"
   },
   {
-    "revision": "8625e2e53af06e384ebb",
+    "revision": "3f293cd9d156f2d9ca81",
     "url": "/react-title-ticker/static/css/main.9ecbd232.chunk.css"
   },
   {
-    "revision": "2237d33fcb0d8237d8a7d7d3eaf3d18c",
+    "revision": "e6ab7bc9c09ac909234f14e4eeeac302",
     "url": "/react-title-ticker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-title-ticker/precache-manifest.d15755d2c89076cc262fcae999f5d810.js"
+  "/react-title-ticker/precache-manifest.1a061f93568d2757cd51485cc67f2652.js"
 );
 
 workbox.clientsClaim();
