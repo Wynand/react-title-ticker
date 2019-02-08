c9d81a024f6028ac5a3aee383a2645bec0438d65
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-02-07 22:37:48 -0600

    the ticking is a little sluggish but it works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TitleTicker from './title-ticker';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <TitleTicker title = "Mary had a little lamb - " step = {1000}/>
         </header>
       </div>
     );
---
+++ src/title-ticker.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function rotateArr(Arr,rot){
+    let len = Arr.length;
+    let r = rot % len;
+    return Arr.slice(r) + Arr.slice(0,r);
+}
+
+class TitleTicker extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {title: props.title, offset: 0, step: props.step}
+    }
+
+    componentDidMount(){
+        this.setState({timer:window.setInterval(()=>this.setState({offset:this.state.offset+1}),this.state.step)});
+    }
+
+    componentWillUnmount(){
+        window.clearInterval(this.state.timer);
+    }
+
+    render(){
+        return ReactDOM.createPortal(
+            <React.Fragment>
+                {rotateArr(this.state.title,this.state.offset)}
+            </React.Fragment>
+        ,document.getElementsByTagName("title")[0]);
+    }
+}
+
+export default TitleTicker;
